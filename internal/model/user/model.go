8dd38f1c01a7ab8029e3b0184c09e265226f5cd1
poopsen8 <vinetolko600@gmail.com>
2025-10-13 22:34:50 +0300

    0.9.2

--- internal/model/user/model.go
+++ internal/model/user/model.go
@@ -15,10 +15,10 @@ type Model struct {
 }
 
 type FullModel struct {
-	ID             int64
-	Username       string
-	CreateAt       time.Time
-	MobileOperator operator.Model
-	TotalSum       int
-	IsTrial        bool
+	ID             int64          `json:"id"`
+	Username       string         `json:"username"`
+	CreateAt       time.Time      `json:"create_at"`
+	MobileOperator operator.Model `json:"operator"`
+	TotalSum       int            `json:"total_sum"`
+	IsTrial        bool           `json:"is_trial"`
 }
