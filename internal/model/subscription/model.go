63c93aa98d7a4906179cb741897233507e668aac
poopsen8 <vinetolko600@gmail.com>
2025-10-14 05:35:04 -0700

    0.9.2

--- internal/model/subscription/model.go
+++ internal/model/subscription/model.go
@@ -15,7 +15,7 @@ type Model struct {
 
 type FullModel struct {
 	User_id    int64       `json:"user_id"`
-	Plan       *plan.Model `json:"plan_id"`
+	Plan       *plan.Model `json:"plan"`
 	CreateAt   time.Time   `json:"create_at"`
 	Expires_at time.Time   `json:"expires_at"`
 	Key        string      `json:"key"`
