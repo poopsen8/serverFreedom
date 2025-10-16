e3f7c071153f130b186b56f78e17491987ee4fd8
poopsen8 <vinetolko500@gmail.com>
2025-10-16 19:53:32 +0300

    0.9.2

--- internal/handler/http/subscription/handler.go
+++ internal/handler/http/subscription/handler.go
@@ -57,6 +57,7 @@ func (h *SubscriptionHandler) Validator(n *y.Notification) {
 func (h *SubscriptionHandler) GetPayment(w http.ResponseWriter, r *http.Request) {
 	var sub subscription.FullModel
 	if err := json.NewDecoder(r.Body).Decode(&sub); err != nil {
+		fmt.Printf("sub: %v\n", sub)
 		fmt.Printf("err.Error(): %v\n", err.Error())
 		writeJSONError(w, httperr.ErrInvalidJSON.StatusRequest, httperr.ErrInvalidJSON.Err.Error())
 		return
